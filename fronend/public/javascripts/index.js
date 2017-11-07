7db9c7f1fda2e44a278dc8ee2eec4a5220a0f448
lcy3155 <315508360@qq.com>
2017-11-07 20:38:20 +0800

    li-5

--- fronend/public/javascripts/index.js
+++ fronend/public/javascripts/index.js
@@ -6,8 +6,6 @@
 //})
 window.onload=function(){
     var iTem=document.querySelectorAll('.item')
-     console.log(iTem)
-    console.log(document.documentElement.clientHeight)
     for(var i=0;i<iTem.length;i++){
         iTem[i].style.height=document.documentElement.clientHeight+'px';
     }
