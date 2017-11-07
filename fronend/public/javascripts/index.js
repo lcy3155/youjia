58388c8912c935afb48ecdcc5dd19b0efb2208db
LYBdabeizei007 <1293649430@qq.com>
2017-11-07 18:36:56 +0800

    3838438

--- fronend/public/javascripts/index.js
+++ fronend/public/javascripts/index.js
@@ -5,16 +5,44 @@
 //    $('.item').height($(window).height())
 //})
 window.onload=function(){
-
     var iTem=document.querySelectorAll('.item')
      console.log(iTem)
     console.log(document.documentElement.clientHeight)
     for(var i=0;i<iTem.length;i++){
         iTem[i].style.height=document.documentElement.clientHeight+'px';
     }
-
+    function  F5() {
+        $.ajax({
+            url:'http://localhost:3000/select',
+            type:'POST',
+            success:function (data) {
+                $('#lunbo').append('<div class="item active">\n' +
+                    '<img src="'+data[0].img+'" alt="...">\n' +
+                    '<div class="carousel-caption">' +
+                    '<h1>'+data[0].title+'</h1>' +
+                    '<h3>'+data[0].detail+'</h3>' +
+                    '<div class="xia"></div>' +
+                    '</div>' +
+                    '</div>')
+                for(i=1;i<data.length;i++){
+                    $('#lunbo').append('<div class="item">' +
+                        '<img src="'+data[i].img+'">' +
+                        '<div class="carousel-caption">' +
+                        '<h1>'+data[i].title+'</h1>' +
+                        '<h3>'+data[i].detail+'</h3>' +
+                        '<div class="xia"></div>' +
+                        '</div>' +
+                        '</div>'
+                    )
+                }
+                for(i=0;i<data.length;i++){
+                    $('#dian').append('<li data-target="#carousel-example-generic" ></li>')
+                }
+            }
+        })
+    }
+    F5()
 }
-
 
 
 
