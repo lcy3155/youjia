71834100047938314f4ae463d38f3a33bcd2fd33
lcy3155 <315508360@qq.com>
2017-11-08 11:09:55 +0800

    li-8

--- fronend/root.js
+++ fronend/root.js
@@ -3,7 +3,7 @@ var mysql=require('mysql')
 var connection=mysql.createPool({
     host:"localhost",
     user:'root',
-    password:'zcx1812281',
+    password:'123456',
     database:'test',
 })
 module.exports = connection;
